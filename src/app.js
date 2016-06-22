e2860559b0c8c3482c03fb7a6fee3753bb0eefe7
Pål Kristian Tønder <pal-kristian.tonder@powel.no>
2016-06-22 14:57:22 +0200

    Change order and hide for presentation

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ export class App {
      // { route: 'welcome-validation', name: 'welcome-validation', moduleId: 'welcome-validation/welcome-validation', nav: true, title: 'Welcome (validated)' },
      // { route: 'users-comp',    name: 'users-comp',   moduleId: './users-comp/users', nav: true, title: 'Github Users (comp)' },
      // { route: 'users-comp/user/:user', name: 'user', moduleId: './users-comp/user', title: 'User' },
+     // { route: 'react',         name: 'react',        moduleId: 'react-in-aurelia/react-in-aurelia', nav: true, title: 'React in Aurelia' }
     ]);
 
     this.router = router;
